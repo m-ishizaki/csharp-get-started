bdfbe08255f8264d53349a1a9abaaec61c117412
is12t <love.ms.is12t@hotmail.co.jp>
2021-07-01 05:55:59 +0900

    fix code format

--- sample/console/src_advanced/ConsoleApp/Program.cs
+++ sample/console/src_advanced/ConsoleApp/Program.cs
@@ -9,16 +9,16 @@ namespace ConsoleApp
             var game1 = new Game(1);
             var game2 = new Game(2);
             Console.WriteLine("数字あて");
-            while(!game1.Cleared || !game2.Cleared)
+            while (!game1.Cleared || !game2.Cleared)
             {
                 Console.Write("数字を入力: ");
                 var line = Console.ReadLine();
-                if(!int.TryParse(line, out var input))
+                if (!int.TryParse(line, out var input))
                 {
                     Console.WriteLine("数字を入力してください");
                     continue;
                 }
-                if(input < 1 || input > 9)
+                if (input < 1 || input > 9)
                 {
                     Console.WriteLine("1-9 の数字を 1 文字入力してください");
                     continue;
